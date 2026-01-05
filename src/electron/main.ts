854e5aa5b908384a492084c231fab55cfb870a12
Yisus Florez <juniniprogramador@outlook.com>
2026-01-04 23:29:17 -0500

    feat: Migrate Electron code to TypeScript and integrate Electron Builder for packaging.

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,17 +1,14 @@
 import { app, BrowserWindow } from 'electron';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import * as path from 'path';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-let mainWindow;
+let mainWindow: BrowserWindow | null;
 
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 1200,
     height: 800,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js'),
+      preload: path.join(app.getAppPath(), 'dist-electron', 'preload.js'),
       contextIsolation: true,
       nodeIntegration: false
     }
@@ -23,7 +20,7 @@ function createWindow() {
     mainWindow.loadURL('http://localhost:4200');
     mainWindow.webContents.openDevTools();
   } else {
-    mainWindow.loadFile(path.join(__dirname, '../../dist/electro-angular/browser/index.html'));
+    mainWindow.loadFile(path.join(__dirname, '../dist/electro-angular/browser/index.html'));
   }
 }
 
--- src/electron/preload.js
+++
@@ -1,5 +0,0 @@
-const {contextBridge, ipcRenderer} = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-  sendMessage : (message) => ipcRenderer.send('message', message)
-});
